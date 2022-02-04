46b36a4a04e103c98907276ad5a0345660e7a661
francoisdomecq <fradomecq@gmail.com>
2022-02-04 15:39:57 +0100

    Image

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.containerRed}></View>
-      <View style={styles.containerYellow}></View>
-      <View style={styles.containerGreen}></View>
+      <Image style={styles.image} source={{uri:'https://www.bpesquet.fr/images/ENSC.jpg'}}/>
+      <Image style={styles.image} source={{uri:'https://www.bpesquet.fr/images/ENSC.jpg'}}/>
+      <Image style={styles.image} source={{uri:'https://www.bpesquet.fr/images/ENSC.jpg'}}/>
     </View>
   )
 }
@@ -15,22 +15,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     justifyContent: 'space-around',
   },
-  containerRed: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-  },
-  containerYellow: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'yellow',
-  },
-  containerGreen: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'green',
-  },
+  image:{
+    height:75,
+    width:108,
+  }
 })
