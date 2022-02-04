20dc9dae7a5aa5875ac274fbdc9ba75f695d28a7
francoisdomecq <fradomecq@gmail.com>
2022-02-04 16:30:37 +0100

    TouchableOpacity

--- App.tsx
+++ App.tsx
@@ -1,77 +1,16 @@
 import React, { Component } from 'react'
-import { FlatList, View, StyleSheet, Text } from 'react-native'
+import { TouchableOpacity, View, StyleSheet, Text } from 'react-native'
 
-const DATA = [
-  {
-    id: 0,
-    firstName: 'Benoit',
-  },
-  {
-    id: 1,
-    firstName: 'Bernard',
-  },
-  {
-    id: 2,
-    firstName: 'Jean-Marc A',
-  },
-  {
-    id: 3,
-    firstName: 'Jérôme',
-  },
-  {
-    id: 4,
-    firstName: 'Hervé',
-  },
-  {
-    id: 5,
-    firstName: 'Jean-Marc S',
-  },
-  {
-    id: 6,
-    firstName: 'Pierre Alexandre',
-  },
-  {
-    id: 7,
-    firstName: 'Edwige',
-  },
-  {
-    id: 8,
-    firstName: 'Baptiste',
-  },
-  {
-    id: 9,
-    firstName: 'Coralie',
-  },
-  {
-    id: 10,
-    firstName: 'Jauze',
-  },
-  {
-    id: 11,
-    firstName: 'Véronique',
-  },
-  {
-    id: 12,
-    firstName: 'Elisabeth',
-  },
-  {
-    id: 13,
-    firstName: 'Liliana',
-  },
-]
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={DATA}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.textFont}>{item.firstName}</Text>
-            </View>
-          )}
-          keyExtractor={(item)=>item.id.toString()}
-        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => alert('Good job !')}
+        >
+          <Text style={styles.textFont}>Hit me like you mean it</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -81,17 +20,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop:20,
-
-  },
-  item: {
-    backgroundColor: 'skyblue',
-    width: '100%',
-    marginTop:5,
-    height:30,
+    alignItems:'center',
     justifyContent:'center'
   },
+  button: {
+    padding:20,
+    width: 300,
+    backgroundColor: 'skyblue',
+    alignItems:'center'
+  },
   textFont:{
-    fontSize:18,
+    fontSize:20,
   }
 })
